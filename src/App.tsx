cf469d0e7e96b46f26a4c2e3ccdb1f073e8c0bd9
KathleenSantos <kathleen.cristine@incentive.me>
2024-03-04 18:41:19 -0300

    style: create theme for dark and light mode and menu toggle

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { router } from './routes'
 import './global.css'
 
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
     <>
+    <ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
       <RouterProvider router={router} />
       <Toaster richColors />
+    </ThemeProvider>
     </>
   )
 }
---
+++ src/components/account-meun.tsx
@@ -0,0 +1,39 @@
+import { Building, ChevronDown, LogOut } from "lucide-react";
+import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuItem, DropdownMenuLabel } from "./ui/dropdown-menu";
+import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
+
+export function AccountMenu() {
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={"outline"} className="flex items-center gap-2 select-none">
+            Pizza shop
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            <span>
+              Kathleen Santos
+            </span>
+            <span className="text-xs font-normal text-muted-foreground">
+              kathleenSantos@google.com
+            </span>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Building className="mr-2 h-4 w-4" />
+            <span>Perfil da loja</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="text-rose-500  dark:text-rose-400">
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Sair</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  )
+}
